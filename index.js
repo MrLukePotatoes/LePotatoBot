d39131b0673d78a20de244ed7c8ed93254d4fbc9
MrLukePotatoes <30947763+MrLukePotatoes@users.noreply.github.com>
2018-05-11 11:28:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setStatus('dnd');
-    client.user.setActivity('with code :3', {type: 'PLAYING'});
+    client.user.setActivity('Luke coding', {type: 'WATCHING'});
 });
 
 client.on('message', async msg => {
     if(msg.author.bot) return;
     
     if (msg.content === '$help') {
-    	msg.channel.send('**__Commands List__** ```[ $server | $info | $ghostview | $help ]``` **__Updated By__** MrLukePotatoes#1488');
+    	msg.channel.send('**__Commands List__** ```[ ./server | ./info | ./discord | ./help ]``` **__Updated By__** MrLukePotatoes#1488');
   	} else
         if (msg.content === 'Luke') {
     	msg.channel.send('You have called him. Please tell, what do you want to talk about?');
   	} else
-        if (msg.content === '$discord') {
+        if (msg.content === './discord') {
     	msg.channel.send('**Discord Link for the bot: Coming soon**');
   	} else
-        if (msg.content === '$server') {
+        if (msg.content === './server') {
     	msg.channel.send('**This is a ZythronPE server! Come and play if you have a time!** ```IP: play.zythronpe.ml | Port: 19132```');
   	} else
-        if (msg.content === '$info') {
+        if (msg.content === './info') {
     	msg.channel.send('**This bot was created on 9 May 2018! Using a code with Discord.js!** ```---``` **Version**: IMB-1.0 (BETA) ```---``` **It is owned by** MrLukePotatoes#1488');
   	}
 });
