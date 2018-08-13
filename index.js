23169040173c700f1dea1169ec659734490abf43
MrLukePotatoes <30947763+MrLukePotatoes@users.noreply.github.com>
2018-08-13 21:29:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
 client.on('ready', () => {
-
     client.user.setStatus('dnd');
-
     client.user.setActivity('and Learning at the same time', {type: 'PLAYING'});
-
 });
-
 client.on('message', async message => {
-
     if (message.author.bot) return;
-
     if (message.content === '>help') {
-
     let helpembed = new Discord.RichEmbed()
-
     .setTitle('- LePotatoBot | Commands -')
     .setDescription('Prefix [ > ]')
     .addField('No commands yet' 'Sorry') 
@@ -25,3 +15,6 @@ client.on('message', async message => {
     .setFooter(`Requested by ${message.author.tag}`)
     return message.channel.send(helpembed)
     };
+});
+
+client.login(process.env.BOT_TOKEN);
