0fc100f00164103ec22333c17bdb888b55a53053
MrLukePotatoes <30947763+MrLukePotatoes@users.noreply.github.com>
2019-01-08 21:57:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ client.on('message', async message => {
     };
     
     if (message.content === 'l>say') {
-     const sayMessage = args.join(" ");
-     message.delete().catch(O_o=>{});
-     message.channel.send(sayMessage);
+      const sayMessage = args.join(" ");
+      message.delete().catch(O_o=>{});
+      message.channel.send(sayMessage);
      };
 });
 
