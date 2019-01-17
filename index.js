05f023d6afb86c013f6005ef6704741b572d54df
MrLukePotatoes <30947763+MrLukePotatoes@users.noreply.github.com>
2019-01-17 19:54:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,46 +3,12 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setStatus('dnd');
-    client.user.setActivity('My Owner', {type: 'WATCHING'});
+    client.user.setActivity('ACTIVE', {type: 'WATCHING'});
 });
 
 client.on('message', async message => {
     if (message.author.bot) return;
     
-    if (message.content === 'l>help') {
-      let helpembed = new Discord.RichEmbed()
-      .setTitle('- LePotatoBot | Commands -')
-      .setDescription('Prefix [ l> ]')
-      .addField('l>gayrate','Shows how much gay you are')
-      .addField('l>hmc','How many crush will you get in aroumd the world')
-      .addField('l>say','Say anything using the command and the bot will say it')
-      .setColor('RANDOM')
-      .setFooter(`Requested by ${message.author.tag}`)
-      return message.channel.send(helpembed);
-    };
-    
-    if (message.content === 'l>gayrate') {
-      let user = message.mentions.users.first() || message.author;
-      let gayembed = new Discord.RichEmbed()
-      .setAuthor(`${message.author.username}`)
-      .addField(`Gay Rate`, `Wow! **${Math.floor(Math.random() * 100)}% Gay**! :gay_pride_flag:`)
-      return message.channel.send(gayembed);
-    };
-    
-    if (message.content === 'l>hmc') {
-      let user = message.mentions.users.first() || message.author;
-      let hmcwygembed = new Discord.RichEmbed()
-      .setAuthor(`${message.author.username}`)
-      .addField(`How Many Crush`, `Around the World **${Math.floor(Math.random() * 7000000000)}** people has a crush on you!`)
-      return message.channel.send(hmcwygembed)
-    };
-    
-    if (message.content === 'l>say') {
-      let sayembed = new Discord.RichEmbed()
-      const sayMessage = args.join(" ");
-      message.delete().catch(O_o=>{});
-      message.channel.send(sayMessage);
-      return message.channel.send(sayembed)
      };
 });
 
